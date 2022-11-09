69166012640350e5a327bf85267543d4198f5506
Talimul212 <islamtalimul0@gmail.com>
2022-11-09 18:51:12 +0600

    modifi itemcard and feature and feedbcak file

--- src/Pages/Home/Feedback/Feedback.js
+++ src/Pages/Home/Feedback/Feedback.js
@@ -4,37 +4,80 @@ import { FaStar } from "react-icons/fa";
 const Feedback = () => {
     return (
         <div>
-        <div className="carousel h-36 w-36 ">
-            <div id="slide1" className="carousel-item relative w-full">
-                {/* <img src={client} className="w-full" alt='' /> */}
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
-                    <a href="#slide2" className="btn btn-circle">❯</a>
+            <h1 className='text-4xl text-center '>Here Is Some Clients<br/>
+                <span className='font-bold' > Smart Feedback</span></h1>
+            <div className="carousel h-80 w-full mb-12 ">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <div className=' bg-zinc-100 p-4 rounded-xl m-6'>
+                        <center> <img src={client} className="w-16 h-16" alt='' /></center>
+                        <div className="review_text text-center">
+                            <ul className="rateing flex justify-center">
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-slate-300'></FaStar></a></li>
+                            </ul>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                            <h5 className=' font-bold'>Rosalina D. Williamson</h5>
+                        </div>
+                    </div>
+                    <div className=' bg-zinc-100 p-4 ml-4 m-6 rounded-xl'>
+                        <center> <img src={client} className="w-16 h-16" alt='' /></center>
+                        <div className="review_text text-center">
+                            <ul className="rateing flex justify-center">
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-slate-300'></FaStar></a></li>
+                            </ul>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                            <h5 className=' font-bold'>Rosalina D. Williamson</h5>
+                        </div>
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-            </div>
-            <div id="slide2" className="carousel-item relative w-full">
-                <img src={client} className="w-full" alt='' />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">❮</a>
-                    <a href="#slide3" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide3" className="carousel-item relative w-full">
-                <img src={client} className="w-full" alt='' />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">❮</a>
-                    <a href="#slide4" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide4" className="carousel-item relative w-full">
-                <img src={client} className="w-full" alt='' />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">❮</a>
-                    <a href="#slide1" className="btn btn-circle">❯</a>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <div className=' bg-zinc-100 p-4 rounded-xl m-6'>
+                        <center> <img src={client} className="w-16 h-16" alt='' /></center>
+                        <div className="review_text text-center">
+                            <ul className="rateing flex justify-center">
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-slate-300'></FaStar></a></li>
+                            </ul>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                            <h5 className=' font-bold'>Rosalina D. Williamson</h5>
+                        </div>
+                    </div>
+                    <div className=' bg-zinc-100 p-4 ml-4 m-6 rounded-xl'>
+                        <center> <img src={client} className="w-16 h-16" alt='' /></center>
+                        <div className="review_text text-center">
+                            <ul className="rateing flex justify-center">
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-yellow-400'></FaStar></a></li>
+                                <li><a href="/"><FaStar className=' text-slate-300'></FaStar></a></li>
+                            </ul>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                            <h5 className=' font-bold'>Rosalina D. Williamson</h5>
+                        </div>
+                    </div>
+
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 };
 
