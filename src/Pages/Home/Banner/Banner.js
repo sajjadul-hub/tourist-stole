9847301d69c358471b07658b14c8e9d0f10c4d76
sajjadul-hub <sajjadulislamsp@gmail.com>
2023-01-13 01:51:19 +0600

    tourist-stole

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -8,13 +8,13 @@ const Banner = () => {
                 <figure><img className='w-full' src={bannerImg} alt="" /></figure>
                 <div className="card-body flex justify-center items-center">
                     <h2 className=' text-2xl lg:text-5xl font-bold text-blue-200 '> Find Your Next Adventure <br />
-                      With Traveller</h2>
+                        With Traveller</h2>
                 </div>
             </div>
             <div className=' flex justify-center mb-2 mt-28'>
                 <img className='h-40 w-40 rounded-full' src={guider} alt=''></img>
             </div>
-            <span className='font-bold text-3xl'>Hi! Is Talimul</span>
+            <span className='font-bold text-3xl'>Hi! It's Sajjadul islam</span>
             <p className=' text-2xl text-center'>
                 <br />
                 I am travel nuts and love visiting new places. I am used my experience to help you find your next adventure. I'll  show you where to go, when to visit, where to stay, and what to pack.</p>
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { Processor } from "postcss";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyDm88zlhK4mA--4s9Qul9ZQwT9PVcmJNWE",
+  authDomain: "tourist-stole.firebaseapp.com",
+  projectId: "tourist-stole",
+  storageBucket: "tourist-stole.appspot.com",
+  messagingSenderId: "637083930662",
+  appId: "1:637083930662:web:061677ea9d35235bef96e7"
 };
 
 // Initialize Firebase
